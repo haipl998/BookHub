b349ee37f01c6f886b3399007ccb9ea82023e38f
haipl998 <phamlonghai013@gmail.com>
2024-08-13 09:02:31 +0700

    feature#3: Create API to Fetch Details of a Specific Book

---
+++ module/book/transport/gin/get_book_handler.go
@@ -0,0 +1,31 @@
+package ginbook
+
+import (
+	"BookHub/module/book/biz"
+	"BookHub/module/book/storage"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+func GetBook(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		store := storage.NewSQLStore(db)
+		business := biz.NewGetBookByIdBiz(store)
+
+		result, err := business.GetBookById(c.Request.Context(), id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"data": result})
+	}
+}
